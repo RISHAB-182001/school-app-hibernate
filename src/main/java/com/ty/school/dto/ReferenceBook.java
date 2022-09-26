990ba4ac0dd46471204a6174102af606e818a97d
RISHAB-182001 <57083330+RISHAB-182001@users.noreply.github.com>
2022-09-26 16:55:45 +0530

    school-app-hibernate

---
+++ src/main/java/com/ty/school/dto/ReferenceBook.java
@@ -0,0 +1,32 @@
+package com.ty.school.dto;
+
+public class ReferenceBook {
+	private int id;
+	private String name;
+	private String author;
+	private String subject;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
